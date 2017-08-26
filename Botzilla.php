d3dabe1116dca3f2678d235f51f1667aae4b0b15
fedehuguet <fede.huguet14@gmail.com>
2017-08-26 16:05:07 -0500

    Responder

--- Botzilla.php
+++ Botzilla.php
@@ -4,14 +4,14 @@
 file_put_contents("fb.txt", file_get_contents("php://input"));
 $fb = file_get_contents("fb.txt");
 
-echo "<pre>";
+//echo "<pre>";
 $fb = json_decode($fb);
 $recipientId = $fb->entry[0]->messaging[0]->sender->id;
-$message = $fb->entry[0]->messaging[0]->message->text;
+//$message = $fb->entry[0]->messaging[0]->message->text;
 
-exec(,$message, -1);
+//exec(,$message, -1);
 
-$token;
+$token="EAADhEjnSxg8BAPi3bOvFyjEKBed9GpugiEI5nsSojB1jNHJh2KZA1Syti1tXQ1LTBwgVZCIqRZC68byp82SpJ8h0hgCZA0ZAdDh2elHyNhqjdG0gZAPWZBRUIAkeYvpjKUwhpHhbZBOz4Az8sfuD31sZCZAxxhliqr4otp7XmLUUZAPeQZDZD";
 
 $data = array(
 	'recipient' => array('id' =>"$recipientId"),
@@ -28,4 +28,4 @@ $options = $options = array(
 
 $context = stream_context_create($options);
 
-print_r($fb);
+$file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=PAGE_ACCESS_TOKEN", false, $context);
