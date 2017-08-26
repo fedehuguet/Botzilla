735544a59701df4073a89f213e174dc920893a45
jolu14 <jolu.mym@gmail.com>
2017-08-26 17:37:51 -0500

    Update BotzillaInterface.php

--- BotzillaInterface.php
+++ BotzillaInterface.php
@@ -1,11 +1,24 @@
 <?php
 
-if (isset($_POST['id']) && isset($_POST['message']) )
+if (isset($_POST['start']))
 {
-    echo shell_exec('python /var/www/html/reddefilantropia.site/public_html/Botzilla/Conversation.py $_POST["id"] $_POST["message"]');
+    if ($_POST['start'] == 1)
+    {
+        echo shell_exec("python /var/www/html/reddefilantropia.site/public_html/Botzilla/StartConversation.py");
+    }
+    else {
+        echo -1;
+    }
 }
 else
 {
-    header("HTTP/1.1 400 Bad Request");
+    if (isset($_POST['id']) &&  isset($_POST['message']) )
+    {
+        echo shell_exec('python /var/www/html/reddefilantropia.site/public_html/Botzilla/Conversation.py $_POST["id"] $_POST["message"]');
+    }
+    else
+    {
+        header("HTTP/1.1 400 Bad Request");
+    }
 }
 ?>
