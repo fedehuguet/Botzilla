527d08f2ccf2cc58108b1eb5394bf84e78cfc7f0
jolu14 <jolu.mym@gmail.com>
2017-08-26 17:31:30 -0500

    Update Conversation.py

--- Conversation.py
+++ Conversation.py
@@ -30,20 +30,6 @@ for row in cur.fetchall():
 
 db.close()
 
-conversation = ConversationV1(
-        username='d19359df-e6fc-4774-8fdc-1e5fea2eee42',
-        password='5FDnK2H87H5d',
-        version='2017-04-21')
-
-workspace_id = '6438675d-c6bf-4e9f-8222-ddc0ed1a4a65'
-response = []
-if mcontext == "-1":
-    response = conversation.message(workspace_id=workspace_id, message_input={
-        'text': sys.argv[2]} )
-else:
-    response = conversation.message(workspace_id=workspace_id, message_input={
-        'text': sys.argv[2]}, context=json.loads(mcontext))
-
 db = MySQLdb.connect(host="filantropiadb.c8sdfecsnkao.us-east-2.rds.amazonaws.com",    # your host, usually localhost
                      user="Jolum",         # your username
                      passwd="FilantropiaDB1234",  # your password
@@ -54,7 +40,45 @@ db = MySQLdb.connect(host="filantropiadb.c8sdfecsnkao.us-east-2.rds.amazonaws.co
 cur = db.cursor()
 
 # Use all the SQL you like
-cur.execute("CALL SP_UpdateContext(" + sys.argv[1] + ", \'" + json.dumps(response['context']) + "\');")
+cur.execute("CALL SP_VerifyConversation(" + sys.argv[1] + ");")
+
+# print all the first cell of all the rows
+
+countConver = 0
+for row in cur.fetchall():
+    countConver = row[0]
+
 db.close()
 
-print(json.dumps(response['output']['text'][0]))
+
+if countConver > 0:
+    conversation = ConversationV1(
+            username='d19359df-e6fc-4774-8fdc-1e5fea2eee42',
+            password='5FDnK2H87H5d',
+            version='2017-04-21')
+
+    workspace_id = '6438675d-c6bf-4e9f-8222-ddc0ed1a4a65'
+    response = []
+    if mcontext == "-1":
+        response = conversation.message(workspace_id=workspace_id, message_input={
+            'text': sys.argv[2]} )
+    else:
+        response = conversation.message(workspace_id=workspace_id, message_input={
+            'text': sys.argv[2]}, context=json.loads(mcontext))
+
+    db = MySQLdb.connect(host="filantropiadb.c8sdfecsnkao.us-east-2.rds.amazonaws.com",    # your host, usually localhost
+                         user="Jolum",         # your username
+                         passwd="FilantropiaDB1234",  # your password
+                         db="Botzilla")        # name of the data base
+
+    # you must create a Cursor object. It will let
+    #  you execute all the queries you need
+    cur = db.cursor()
+
+    # Use all the SQL you like
+    cur.execute("CALL SP_UpdateContext(" + sys.argv[1] + ", \'" + json.dumps(response['context']) + "\');")
+    db.close()
+
+    print(json.dumps(response['output']['text'][0]))
+else:
+    print("error")
