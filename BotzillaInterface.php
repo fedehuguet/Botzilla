bab696e81b5a9f4d10370852285366c4a05c6a5e
jolu14 <jolu.mym@gmail.com>
2017-08-26 17:20:35 -0500

    Update BotzillaInterface.php

--- BotzillaInterface.php
+++ BotzillaInterface.php
@@ -1,6 +1,6 @@
 <?php
 //========================================================================================================================
-if (isset($_POST['id']) &&  isset($_POST['message']) )
+if (isset($_POST['id']) && isset($_POST['message']) )
 {
     echo shell_exec("python /var/www/html/reddefilantropia.site/public_html/Botzilla/Conversation.py $_POST['id'] $_POST['message']");
 }
