f930e9e03e94575fbacc28ab3c04b9513e7990c8
jolu14 <jolu.mym@gmail.com>
2017-08-26 15:20:01 -0500

    Create StartConversation.py

---
+++ StartConversation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import MySQLdb
+
+db = MySQLdb.connect(host="13.58.25.89",    # your host, usually localhost
+                     user="root",         # your username
+                     passwd="",  # your password
+                     db="RFDBV1")        # name of the data base
+
+# you must create a Cursor object. It will let
+#  you execute all the queries you need
+cur = db.cursor()
+
+# Use all the SQL you like
+cur.execute("SP_AddConversation();")
+
+# print all the first cell of all the rows
+for row in cur.fetchall():
+    print row[0]
+
+db.close()
