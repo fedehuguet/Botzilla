523ce8f782701262fc853a2682ce073a916d92ae
jolu14 <jolu.mym@gmail.com>
2017-08-27 00:15:29 -0500

    Update Dashboard.php

--- Dashboard.php
+++ Dashboard.php
@@ -2,7 +2,7 @@
 include "Tools.php";
 
 //========================================================================================================================
-//header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: PUT, POST, DELETE, OPTIONS');
 //header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
 header("Content-Type: application/json", true);
