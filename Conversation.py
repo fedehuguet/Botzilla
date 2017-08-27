f9ed032697fa9b48bb712387401f3ee2bafd8ee9
jolu14 <jolu.mym@gmail.com>
2017-08-27 10:22:52 -0500

    Update Conversation.py

--- Conversation.py
+++ Conversation.py
@@ -82,7 +82,7 @@ if countConver > 0:
     db.close()
     
     
-    if json.dumps(response['output']['text'][0]) == '¡Me da gusto haberle ayudado!':
+    if json.dumps(response['output']['text'][0][0]) == "~":
       b = MySQLdb.connect(host="filantropiadb.c8sdfecsnkao.us-east-2.rds.amazonaws.com",    # your host, usually localhost
                          user="Jolum",         # your username
                          passwd="FilantropiaDB1234",  # your password
@@ -96,7 +96,7 @@ if countConver > 0:
       proce = "CALL SP_SetSovedUnSolved(" + sys.argv[1] + ",'Sí');"
       cur.execute(proce)
       db.close()
-    elif json.dumps(response['output']['text'][0]) == 'Lo siento, espero poderle ayudar en un futuro.':
+    elif json.dumps(response['output']['text'][0][0]) == "^":
       b = MySQLdb.connect(host="filantropiadb.c8sdfecsnkao.us-east-2.rds.amazonaws.com",    # your host, usually localhost
                          user="Jolum",         # your username
                          passwd="FilantropiaDB1234",  # your password
